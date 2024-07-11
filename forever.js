d1eee5eeed066a657dfb62078629376f615dc2c3
ian gong <200804632@qq.com>
2024-07-11 12:41:56 +0800

    添加通过重启进程的方式重连

---
+++ forever.js
@@ -0,0 +1,45 @@
+//简单进程守护 
+import { spawn } from 'child_process';
+import account from './account';
+
+async function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+(async () => {
+    async function runCmd() {
+        return new Promise((resolve, reject) => {
+            const childProcess = spawn("node", ["app.js"], {
+                "cwd": process.cwd(),
+                "shell": true,
+                "stdio": "inherit",
+                env: {
+                    ...process.env,
+                    "XDDQ-ASSISTANT-USE-FOREVER": "true"
+                }
+            });
+
+            childProcess.on("exit", (code, signal) => {
+                console.log('子进程以' + `code ${code}和signal ${signal} 退出`);
+                resolve(code);
+            });
+
+            childProcess.on("error", (err) => {
+                console.error(err);
+                reject(err);
+            })
+        });
+    }
+
+    while (true) {
+        try {
+            await runCmd();
+        } catch (error) {
+            console.error(error);
+        }
+        await sleep(account.reconnectInterval);
+    }
+
+})();
