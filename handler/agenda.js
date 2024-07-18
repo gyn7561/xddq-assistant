aef65d906584f39dcbd218112ef70bab9661415f
ian gong <200804632@qq.com>
2024-07-18 11:29:30 +0800

    修复游历问题，斗法优化

--- handler/agenda.js
+++ handler/agenda.js
@@ -12,7 +12,7 @@ function agenda() {
     if (account.switch.herorank) {
         // 光速抢镑任务在周一0:05分开始执行
         const delay = getMillisecondsUntilNextMonday();
-        console.log(`[群英镑] 距离周一还有 ${delay / 1000 } 秒`);
+        console.log(`[群英镑] 距离周一还有 ${delay / 1000} 秒`);
         setTimeout(() => {
             logger.info("[群英镑] 光速抢榜一");
             TaskManager.instance.add(Herorank.S_HERORANK_BUY_ENERGY());
@@ -23,9 +23,12 @@ function agenda() {
 
 function consumBag() {
     async function checkBag() {
-        // 斗法券 > 0 的时候自动斗法
+        // 斗法券 > 2 的时候自动斗法  如果是在 23:45 ~ 00:00 之间，只要有券就斗 抢排名
         const fightTicket = AttributeManager.instance.findItemById(100026);
-        if (fightTicket.num > 0) {
+        let now = new Date();
+        let miniutesInThisDay = now.getHours() * 60 + now.getMinutes();
+        let isInTime = miniutesInThisDay >= 0 && miniutesInThisDay <= 60 * 23 + 45;// 23:45 ~ 00:00
+        if ((fightTicket.num > 0 && !isInTime) || fightTicket.num > 2) {
             logger.info(`还剩 ${fightTicket.num} 张券`);
             await TaskManager.instance.add(Attribute.FetchBattle());
             await new Promise((resolve) => setTimeout(resolve, 1000));
@@ -48,7 +51,7 @@ function getMillisecondsUntilNextMonday() {
     const currentDay = now.getDay(); // 当前是星期几（0 表示星期日，1 表示星期一，依此类推）
     const nowHours = now.getHours();
     const nowMinutes = now.getMinutes();
-    
+
     // 计算距离下一个周一的天数
     let daysUntilNextMonday = (8 - currentDay) % 7;
 
--- handler/destiny.js
+++ handler/destiny.js
@@ -10,6 +10,11 @@ function handlerDestiny(body) {
     if (power > 0) {
         logger.info(`[Server] [仙友游历] [Power: ${power}] 一键游历`);
         TaskManager.instance.add(Destiny());
+    } else {
+        //半小时会恢复体力，半小时后添加任务 
+        setTimeout(() => {
+            TaskManager.instance.add(Destiny());
+        }, 60 * 1000 * 30);
     }
 }
 
